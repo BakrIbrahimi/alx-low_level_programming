e2bca0bf211018e6bf846d3b8fca0c663d50688b
Bakr Ibrahimi <bakribra5@gmail.com>
2023-03-17 17:34:26 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int d = 0;
+
+while (d <= 9)
+{
+putchar (d + '0');
+++d;
+}
+putchar('\n');
+return (0);
+}
