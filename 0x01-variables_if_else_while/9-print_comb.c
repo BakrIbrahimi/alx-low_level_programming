807e67c6999798ba646dddb6f71df7591b154897
Bakr Ibrahimi <bakribra5@gmail.com>
2023-03-17 17:55:17 +0100

    print number 0, 1, ...9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print numbers 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int d = 0;
+while (d <= 9) 
+{
+putchar (d + '0');
+if (d != 9)
+{
+putchar (',');
+putchar (' ');
+}
+++d;
+}
+putchar('\n');
+return (0);
+}
