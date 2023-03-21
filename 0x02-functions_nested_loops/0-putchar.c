4c3509ccac223baaf8e04169088ecc01259ddb7b
Bakr Ibrahimi <bakribra5@gmail.com>
2023-03-21 08:49:03 +0000

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+__putchar('_');
+__putchar('p');
+__putchar('u');
+__putchar('c');
+__putchar('h');
+__putchar('a');
+__putchar('r');
+__putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My readme on C : 0x02-functions_nested_loops
