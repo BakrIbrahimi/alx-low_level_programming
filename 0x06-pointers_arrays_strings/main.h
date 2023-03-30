05136bd4eff47b44d7b1c2374b9332c479bf4365
Bakr Ibrahimi <bakribra5@gmail.com>
2023-03-30 16:10:31 +0000

    task 12

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,4 +16,7 @@ char *rot13(char *);
 void print_number(int n);
 void rev_string(char *n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_line(char *c, int s, int l);
+void print_buffer(char *b, int size);
+
 #endif
