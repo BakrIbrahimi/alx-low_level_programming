e189b972913f7207a4fc6dfd3570be0e79e4711e
Bakr Ibrahimi <bakribra5@gmail.com>
2023-04-04 01:42:12 +0000

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array of 8 strings
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (i < 64)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			j = i;
+			_putchar('\n');
+		}
+
+		_putchar(a[i / 8][i - j]);
+		i++;
+	}
+	_putchar('\n');
+}
