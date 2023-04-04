5bcd5512e0a64d7637685ef9c28057f30f6bfa19
Bakr Ibrahimi <bakribra5@gmail.com>
2023-04-04 01:17:39 +0000

    task 0

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My readme on C : 0x07-pointers_arrays_strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+ 
+ #include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
+
+ #endif
