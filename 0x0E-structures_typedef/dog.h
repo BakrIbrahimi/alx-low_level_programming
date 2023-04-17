4515a16d7a815ef8f72a48337475375fd5448bec
Bakr Ibrahimi <bakribra5@gmail.com>
2023-04-17 22:15:11 +0000

    task1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdlib.h>
+
 /**
  * struct dog - Struct dog
  * @name: name
@@ -15,5 +17,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /*DOG_H*/
