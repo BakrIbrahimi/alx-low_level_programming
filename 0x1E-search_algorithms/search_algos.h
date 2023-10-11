a2bb61b0140862fa189146a7a1a295da347954b3
Bakr Ibrahimi <bakribra5@gmail.com>
2023-10-11 18:43:14 +0100

    add task10

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,6 +9,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int binary_search_recursive(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 
 #endif /* _SORTING_ALGOS_H_ */
